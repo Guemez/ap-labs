3fc775a81e60b38ad7093cb8b50dacf3b4b0be16
Guemez <danielaguemez@gmail.com>
2019-02-05 17:11:36 -0700

    string lib

--- labs/lab0.2/str-main.c
+++ labs/lab0.2/str-main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-main(){
-    printf("Initial lenght: ");
-    printf("New String: ");
+int strlen(char *str);
+char* stradd(char *origin, char *addition);
+int strfind(char *origin, char *substr);
+
+
+int main( int argc, char *argv[]){
+    if(argc == 1){
+        printf("no parameters provided");
+	return 0;
+    }
+    int i = strlen(argv[1]);
+    printf("Initial lenght: %d\n", i);
+    char *s = stradd(argv[1], argv[2]);
+    printf("New string: %s\n", s);
+    int j = strfind(argv[1], argv[3]);
+    printf("Substring  was found: ");
+    if(j == 1){
+	printf("yes\n");
+    }
+    else{
+	printf("no\n");
+    }
 }
--- labs/lab0.2/strlib.c
+++ labs/lab0.2/strlib.c
@@ -8,19 +8,49 @@ int strlen(char *str){
     return count;
 }
 
-/**char stradd(char *origin, char *addition){
-
-}*/
+char* stradd(char *origin, char *addition){
+    int l = strlen(origin) + strlen(addition) + 1;
+    char *p = malloc(l);
+    int i = 0;
+    while (i < strlen(origin)){
+        p[i] = origin[i];
+        i++;
+    }
+    int j = 0;
+    while(j < strlen(addition)){
+	p[i] = addition[j];
+	i++;
+	j++;
+    }
+    p[i] = '\0';
+    return p;
+}
 
 int strfind(char *origin, char *substr){
-
-}
-
-int main( int argc, char *argv[]){
-    if(argc == 1){
-        printf("no parameters provided");
-	return 0;
+    int len1 = strlen(origin);
+    int len2 = strlen(substr);
+    //printf("str1: %s, str2: %s\n", origin, substr);
+    //printf("l1: %d, l2: %d\n", len1, len2);
+    int i = 0;
+    int j = 0;
+    int flag = 0;
+    while(i < len1){
+	if(*(origin + i) == *(substr + j)){
+	    flag = 1;
+	    i++;
+	    j = i;
+	    for (int k = 1; k < len2 - 1; k++){
+		if(*(origin + i) != *(substr + k)){
+		    flag = 0;
+		    i = j;
+		    break;
+		}
+	    }
+	    
+	}
+	i++;
     }
-    int i = strlen(argv[1]);
-    printf("len: %d\n", i);
+    return flag;
 }
+
+
