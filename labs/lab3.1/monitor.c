777467c18c4e44a7acfb24740ac910f4f93466bd
Guemez <danielaguemez@gmail.com>
2019-03-19 19:16:19 -0700

    implemented monitor.c that detects create, delete and renamed files and directories

--- labs/lab3.1/monitor.c
+++ labs/lab3.1/monitor.c
@@ -1,44 +1,77 @@
 #include <sys/inotify.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include "logger.h"
 #define _XOPEN_SOURCE 500   /* See feature_test_macros(7) */
 #include <ftw.h>
 
-int nftw(const char *dirpath,
-        int (*fn) (const char *fpath, const struct stat *sb,
-                   int typeflag, struct FTW *ftwbuf),
-        int nopenfd, int flags);
+int inotifyFd, wd;
 
+int c1, c2;
+int cook;
+char *path;
+
+static int
+display_info(const char *fpath, const struct stat *sb,
+             int tflag, struct FTW *ftwbuf)
+{
+    if( tflag == FTW_D){
+        wd = inotify_add_watch(inotifyFd, fpath, IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO );
+        if (wd == -1)
+            errorf("inotify_add_watch");
+        //infof("Watching %s using wd %d\n", fpath, wd);
+    }
+
+    return 0;           /* To tell nftw() to continue */
+}
+
+int callnftw(){
+    if (nftw(path, display_info, 20, 0)
+            == -1) {
+             errorf("nftw");
+             exit(0);
+            }
+}
 
 static void             /* Display information from inotify_event structure */
 displayInotifyEvent(struct inotify_event *i)
 {
-    printf("    wd =%2d; ", i->wd);
-    if (i->cookie > 0)
-        printf("cookie =%4d; ", i->cookie);
 
-    printf("mask = ");
-    if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
-    if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
-    if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
-    if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
-    if (i->mask & IN_CREATE)        printf("IN_CREATE ");
-    if (i->mask & IN_DELETE)        printf("IN_DELETE ");
-    if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
-    if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
-    if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
-    if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
-    if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
-    if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
-    if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
-    if (i->mask & IN_OPEN)          printf("IN_OPEN ");
-    if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
-    if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+    if (i->cookie > 0){
+        //warnf("cookie =%4d; ", i->cookie);
+    }
+    if (i->mask & IN_CREATE){
+        infof("CREATED");
+        if (i->len > 0)
+            infof("        name = %s\n", i->name);
+    }
+    if (i->mask & IN_DELETE){
+        infof("DELETED");
+        if (i->len > 0)
+            infof("        name = %s\n", i->name);
+    }
+    if (i->mask & IN_MOVED_FROM){
+	cook = i->cookie;
+        //printf("%i", cook);
+    }
+    if (i->mask & IN_MOVED_TO){   
+	if(cook == i->cookie){
+	    //printf("%i", cook);
+	    infof("RENAMED");
+	cook = 0;
+	if (i->len > 0)
+            infof("        name = %s\n", i->name);
+	}
+    }
+    callnftw();
+
     printf("\n");
 
-    if (i->len > 0)
-        printf("        name = %s\n", i->name);
+    /*if (i->len > 0)
+        printf("        name = %s\n", i->name);*/
 }
 
 #define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
@@ -46,33 +79,33 @@ displayInotifyEvent(struct inotify_event *i)
 int
 main(int argc, char *argv[])
 {
-    int inotifyFd, wd, j;
+    
+    int j;
     char buf[BUF_LEN] __attribute__ ((aligned(8)));
     ssize_t numRead;
     char *p;
+    //char *fp;
     struct inotify_event *event;
 
     if (argc < 2 || strcmp(argv[1], "--help") == 0)
         errorf("%s pathname...\n", argv[0]);
 
+
     inotifyFd = inotify_init();                 /* Create inotify instance */
     if (inotifyFd == -1)
         errorf("inotify_init");
 
-    /* For each command-line argument, add a watch for all events */
-
-    for (j = 1; j < argc; j++) {
-        wd = inotify_add_watch(inotifyFd, argv[j], IN_ALL_EVENTS);
+    path = argv[1];
+	
+        wd = inotify_add_watch(inotifyFd, path, IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO );
         if (wd == -1)
             errorf("inotify_add_watch");
 
-        printf("Watching %s using wd %d\n", argv[j], wd);
-    }
+	callnftw();
 
 
 
-
-
+    
 
     for (;;) {                                  /* Read events forever */
         numRead = read(inotifyFd, buf, BUF_LEN);
@@ -82,7 +115,7 @@ main(int argc, char *argv[])
         if (numRead == -1)
             errorf("read");
 
-        printf("Read %ld bytes from inotify fd\n", (long) numRead);
+        //printf("Read %ld bytes from inotify fd\n", (long) numRead);
 
 
         /* Process all of the events in buffer returned by read() */
@@ -91,9 +124,10 @@ main(int argc, char *argv[])
             event = (struct inotify_event *) p;
             displayInotifyEvent(event);
 
+
+
             p += sizeof(struct inotify_event) + event->len;
         }
     }
     exit(0);
-    //exit(EXIT_SUCCESS);
 }
